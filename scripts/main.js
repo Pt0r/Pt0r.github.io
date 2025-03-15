2ce5578582c8afae724bae0c402e3b4c9e0f77ac
Pt0r <ptorpare@gmail.com>
2025-03-16 00:04:54 +0100

    animated wooo

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,68 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext("2d");
     canvas.width = this.window.innerWidth-20;
     canvas.height = this.window.innerHeight-20;
-    const catImg = new Image();
-    catImg.src = "/content/IMG_5283.gif";
 
-    canvas.addEventListener('mousemove', e => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const fps = 60;
+    let counter = 0;
 
-        let x = e.clientX - canvas.offsetLeft;
-        let y = e.clientY - canvas.offsetTop;
+    const catImg1 = new Image();
+    const catImg2 = new Image();
+    const catImg3 = new Image();
+    const catImg4 = new Image();
+    catImg1.src = "/content/cat_frame1.png";
+    catImg2.src = "/content/cat_frame2.png";
+    catImg3.src = "/content/cat_frame3.png";
+    catImg4.src = "/content/cat_frame4.png";
 
-        ctx.drawImage(catImg, x + 4, y + 4, 50, 50);
-    });
+    const frames = [
+        catImg1,
+        catImg2,
+        catImg3,
+        catImg4
+    ]
+
+    let currentFrame = 0;
+ 
+    let mouseX = 0;
+    let mouseY = 0;
+    let mouseInside = false;
+
+    onmousemove = function(e){
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }
+
+    function draw() {
+        setTimeout(() => {
+            if(counter == 60) {
+                counter = 0;
+            }
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+            let x = mouseX - canvas.offsetLeft;
+            let y = mouseY - canvas.offsetTop;
+    
+            ctx.drawImage(frames[currentFrame], x - 20, y - 15);
+            if(mouseInside) {
+                counter++;
+                if(counter % 10 == 0){
+                    currentFrame = (currentFrame + 1) % frames.length;
+                }
+                window.requestAnimationFrame(draw);
+            } else {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+          }, 1000 / fps);
+    }
 
     canvas.addEventListener('mouseleave', e => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    })
+        mouseInside = false;
+    });
+
+    canvas.addEventListener('mouseenter', e => {
+        mouseInside = true;
+        window.requestAnimationFrame(draw);
+    });
 });
 
